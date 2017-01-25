714305d8d04c4b113b64865c58741391141e832d
Toba Adewuyi <toba.adewuyi@yahoo.com>
2017-01-25 19:15:44 +0100

    fixed 3s and 5s
    The program now checks up till 1000, while not including 1000 in the
    summation. This is because the probles says "below"

--- 3sand5s.js
+++ 3sand5s.js
@@ -6,9 +6,11 @@ Find the sum of all the multiples of 3 or 5 below 1000. */
 
 var total = 0;
 
-for (var i=0; i<=100; i++){
+for (var i=0; i<1000; i++){
   if((i%3 === 0)||(i%5 === 0)){
-    total +=i;
+    total = total + i;
   }
   
 }
+
+console.log(total);
